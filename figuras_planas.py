939553f946a43263e8dfa6a9a98c64887f0e8ab4
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-06-28 08:37:49 -0300

    Atualização

--- figuras_planas.py
+++ figuras_planas.py
@@ -1,14 +1,27 @@
 import unicodedata
 qual_figura = input("Digite qual a figura que você deseja calcular a área: ")
 figura_limpa = unicodedata.normalize('NFKD', qual_figura).encode('ASCII', 'ignore').decode('ASCII').upper()
-base_altura = ["RETANGULO", "QUADRADO", "PARALELOGRAMO", "TRIANGULO",]
-diferentes = ["TRAPEZIO, 'LOSANGO"]
+base_altura = ["RETANGULO", "QUADRADO", "PARALELOGRAMO", "TRIANGULO","TRAPEZIO"]
+diferentes = ["LOSANGO", "CIRCULO", "PENTAGONO", "HEXAGONO",]
 if figura_limpa in base_altura:
-    base = float(input("Digite o valor da base da figura: "))
-    altura = float(input("Digite o valor da altura da figura(se for um quadrado apenas repita o valor da base): "))
+    base = float(input("Digite o valor da base da figura(se for um trapézio, digite a base maior): "))
+    altura = float(input("Digite o valor da altura da figura(se for um quadrado repita o valor da base): "))
     if base<=0 or altura<=0:
         print("Os valores de base ou altura não podem ser negativos")
     else:
         if figura_limpa == "RETANGULO" or figura_limpa == "PARALELOGRAMO" or figura_limpa== "QUADRADO":
             formula = (base*altura)
             print(formula)
+        elif figura_limpa == "TRIANGULO":
+            formula = (base*altura)/2
+            print(formula)
+        else:
+            base_menor = float(input("Digite a base menor: "))
+            formula = ((base+base_menor)*altura)/2
+            print(formula)
+elif figura_limpa == "CIRCULO":
+    raio = float(input("Digite qual é o raio da figura"))
+    pi = 3.14159
+    formula = (raio**2)*pi
+    print(formula)
+
