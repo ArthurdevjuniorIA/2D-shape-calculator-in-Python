d9b1fe1e76e59798f8e471df607ea371aca0b73c
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-27 07:55:36 -0300

    fix: adding the try and except so the code dosent break when the shape isnt in the lists

--- code/figuras_planas.py
+++ code/figuras_planas.py
@@ -24,59 +24,66 @@ def conversor_de_unidades(valor):
     else:
         pass
     return apenas_valor
-
-if figura_limpa in base_altura:
-    medida_1 = input("Digite o valor da base da figura(se for um trapézio, digite a base maior): ")
-    apenas_valor, unidade_de_medida = medida_1.split()
-    apenas_valor = float(apenas_valor)
-    apenas_valor = conversor_de_unidades(apenas_valor)
-    medida_2 = input("Digite o valor da altura da figura(se for um quadrado repita o valor da base): ")
-    apenas_valor_2, unidade_de_medida = medida_2.split()
-    apenas_valor_2 = float(apenas_valor_2)
-    apenas_valor_2 = conversor_de_unidades(apenas_valor_2)
-    if apenas_valor<=0 or apenas_valor_2<=0:
-        print("Os valores de base ou altura não podem ser negativos")
-    else:
-        if figura_limpa == "RETANGULO" or figura_limpa == "PARALELOGRAMO" or figura_limpa== "QUADRADO":
-            formula = float(apenas_valor*apenas_valor_2)
-        elif figura_limpa == "TRIANGULO":
-            formula = (apenas_valor)*(apenas_valor_2)/2
+try:
+    if figura_limpa in base_altura:
+        medida_1 = input("Digite o valor da base da figura(se for um trapézio, digite a base maior): ")
+        apenas_valor, unidade_de_medida = medida_1.split()
+        apenas_valor = float(apenas_valor)
+        apenas_valor = conversor_de_unidades(apenas_valor)
+        medida_2 = input("Digite o valor da altura da figura(se for um quadrado repita o valor da base): ")
+        apenas_valor_2, unidade_de_medida = medida_2.split()
+        apenas_valor_2 = float(apenas_valor_2)
+        apenas_valor_2 = conversor_de_unidades(apenas_valor_2)
+        if apenas_valor<=0 or apenas_valor_2<=0:
+            print("Os valores de base ou altura não podem ser negativos")
         else:
-            medida_3 = input("Digite a base menor: ")
-            apenas_valor_3, unidade_de_medida = medida_3.split()
-            apenas_valor_3 = conversor_de_unidades(apenas_valor_3)
-            formula = ((float(apenas_valor)+float(apenas_valor_3))*float(apenas_valor_2))/2
+            if figura_limpa == "RETANGULO" or figura_limpa == "PARALELOGRAMO" or figura_limpa== "QUADRADO":
+                formula = float(apenas_valor*apenas_valor_2)
+            elif figura_limpa == "TRIANGULO":
+                formula = (apenas_valor)*(apenas_valor_2)/2
+            else:
+                medida_3 = input("Digite a base menor: ")
+                apenas_valor_3, unidade_de_medida = medida_3.split()
+                apenas_valor_3 = conversor_de_unidades(apenas_valor_3)
+                formula = ((float(apenas_valor)+float(apenas_valor_3))*float(apenas_valor_2))/2
 
-elif figura_limpa in diferentes:
-    if figura_limpa == "CIRCULO":
-        medida_1 = input("Digite qual é o raio da figura: ")
-        apenas_valor, unidade_de_medida = medida_1.split()
-        pi = 3.14159
-        apenas_valor = conversor_de_unidades(apenas_valor)
-        formula = (apenas_valor**2)*pi
-    else:
-        medida_1 = input("Digite o valor da diagonal maior: ")
-        apenas_valor, unidade_de_medida = medida_1.split()
-        apenas_valor = conversor_de_unidades(apenas_valor)
-        medida_2 = input("Digite o valor da diagonal menor: ")
-        apenas_valor_2, unidade_de_medida = medida_2.split()
-        apenas_valor_2 = conversor_de_unidades(apenas_valor_2)
-        formula = (float(apenas_valor)*float(apenas_valor_2))/2
+    elif figura_limpa in diferentes:
+        if figura_limpa == "CIRCULO":
+            medida_1 = input("Digite qual é o raio da figura: ")
+            apenas_valor, unidade_de_medida = medida_1.split()
+            pi = 3.14159
+            apenas_valor = conversor_de_unidades(apenas_valor)
+            formula = (apenas_valor**2)*pi
+        else:
+            medida_1 = input("Digite o valor da diagonal maior: ")
+            apenas_valor, unidade_de_medida = medida_1.split()
+            apenas_valor = conversor_de_unidades(apenas_valor)
+            medida_2 = input("Digite o valor da diagonal menor: ")
+            apenas_valor_2, unidade_de_medida = medida_2.split()
+            apenas_valor_2 = conversor_de_unidades(apenas_valor_2)
+            formula = (float(apenas_valor)*float(apenas_valor_2))/2
 
-elif figura_limpa in gono:
-    if figura_limpa == "PENTAGONO":
-        medida_1 = input("Digite o valor da apótema(se não souber digite NAO): ")
-        apenas_valor, unidade_de_medida = medida_1.split()
-        apenas_valor = conversor_de_unidades(apenas_valor)
-        medida_2 = input("Digite o perimetro(se não souber digite NAO): ")
-        apenas_valor_2, unidade_de_medida = medida_2.split()
-        apenas_valor_2 = conversor_de_unidades(apenas_valor_2)
-        formula = (float(apenas_valor)*float(apenas_valor_2))/2
-    else:
-        lado = input("Digite o valor do lado do hexágono: ")
-        apenas_valor, unidade_de_medida = lado.split()       
-        apenas_valor = conversor_de_unidades(float(apenas_valor))
-        raiz_3 = 3**(0.5)
-        formula = (3*(apenas_valor**2)*raiz_3)/2
-
-print(f"o Valor da área do {qual_figura} é {formula} m²")
+    elif figura_limpa in gono:
+        if figura_limpa == "PENTAGONO":
+            medida_1 = input("Digite o valor da apótema(se não souber digite NAO): ")
+            apenas_valor, unidade_de_medida = medida_1.split()
+            apenas_valor = conversor_de_unidades(apenas_valor)
+            medida_2 = input("Digite o perimetro(se não souber digite NAO): ")
+            apenas_valor_2, unidade_de_medida = medida_2.split()
+            apenas_valor_2 = conversor_de_unidades(apenas_valor_2)
+            formula = (float(apenas_valor)*float(apenas_valor_2))/2
+        else:
+            lado = input("Digite o valor do lado do hexágono: ")
+            apenas_valor, unidade_de_medida = lado.split()       
+            apenas_valor = conversor_de_unidades(float(apenas_valor))
+            raiz_3 = 3**(0.5)
+            formula = (3*(apenas_valor**2)*raiz_3)/2
+    print(f"o Valor da área do {qual_figura} é {formula} m²")
+except:
+    print("A figura desejada não está registrada! Por favor tente uma figura registrada que são: ")
+    for registrada in base_altura:
+        print(registrada)
+    for tem_essas in gono:
+        print(tem_essas)
+    for apenas_essas in diferentes:
+        print(apenas_essas)
